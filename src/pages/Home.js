7f994a1e41f57255157211387cf81f269bad63ab
Marinkie <69327534+Marinkiet@users.noreply.github.com>
2021-12-02 16:54:21 +0200

    search input with icons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,8 @@ import './home.css';
 import {Link} from "react-router-dom";
 import AppsIcon from "@material-ui/icons/Apps";
 import {Avatar} from "@material-ui/core";
+import Search from './Search';
+
 function Home() {
     return (
         <div className="home">
@@ -12,26 +14,33 @@ function Home() {
 
             
                 <div className='home_headerLeft'>
-                {/*Link About */}
-                <Link to='/about'>About</Link>
-                 {/*Link Store*/}
-                 <Link to='/store'>Store</Link>
+                    {/*Link About */}
+                    <Link to='/about'>About</Link>
+                    {/*Link Store*/}
+                    <Link to='/store'>Store</Link>
                 </div>
                 <div className='home_headerRight'>
-                 {/*Link Gmail */}
-                 <Link to='/gmail'>Gmail</Link>
-                 {/*Link Images*/}
-                 <Link to='/images'>Images</Link>
-                 {/*Link All Apps Icon*/}
-                <AppsIcon/>
-                 {/*Link Avatar*/}
-                 <Avatar/>
-                </div>
-                </div>
-                <div className='home_body'>
-
+                    {/*Link Gmail */}
+                    <Link to='/gmail'>Gmail</Link>
+                    {/*Link Images*/}
+                    <Link to='/images'>Images</Link>
+                    {/*Link All Apps Icon*/}
+                    <AppsIcon/>
+                    {/*Link Avatar*/}
+                    <Avatar/>
                 </div>
             </div>
+
+            <div className='home_body'>
+                        <img 
+                            src = " https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt="glogo"/>
+                  
+                 <div className="home_inputContainer"/>
+                     <Search/>
+                </div>
+            </div>
+      
+          
     )
 }
 
---
+++ src/pages/Search.js
@@ -0,0 +1,36 @@
+import React,{useState} from 'react';
+import './Search.css';
+import SearchIcon from "@material-ui/icons/Search";
+import MicIcon from "@material-ui/icons/Mic";
+import {Button} from "@material-ui/core"
+
+function Search() {
+//keep track of seached input
+//use state hook
+const [input,setInput] = useState("");
+
+    const search = e =>{
+        e.preventDefault();
+    }
+
+
+    return (
+        <div className="search">
+        <div className="search_input">
+            <SearchIcon className = "search_inputIcon"/>
+            <input value={input} onChange = {e => setInput(e.target.value)}/>
+            <MicIcon/>
+        </div>
+        <div className="search_buttons">
+            <Button  onClick={Search} varient="outlined">
+                Google Search
+            </Button>
+            <Button varient="outlined">
+                I'm feeling Lucky ;)
+            </Button>
+        </div>
+        </div>
+    )
+}
+
+export default Search
